18b1ea6de8df4708958a9e886b7aaef2a0e1fa05
mannan-2024 <shaikh.mannan.1978@gmail.com>
2025-11-18 13:59:38 +0600

    database connected

--- index.js
+++ index.js
@@ -1,47 +1,43 @@
-// const express = require("express") old version
-import express from "express" // new method
+import express from "express";
+import mongoose from "mongoose";
+import User from "./models/user.model.js";
 
 const app = express();
+const PORT = 5000;
+
+const MONGO_URI = "mongodb+srv://test-project:Test12345678@cluster0.x8bbexo.mongodb.net/"
+
+const connectDB = async () => {
+    try {
+        mongoose.connect(MONGO_URI).then(()=>{
+            console.log("Database connected!")
+        })
+    } catch (error) {
+        console.log("database connection error : ", error)
+    }
+}
+
+await connectDB()
 
 app.use(express.json());
 
-app.get("/", (req, res)=>{
-    const data = {
-        name: "Shahin",
-        location: "Faridpur"
-    }
-    res.status(200).json(data)
+app.post("/user/register", async (req, res) => {
+    const {name, email, password} = req.body;
+    
+    if(!name) return res.status(400).json({message: "Name is Required!"});
+    if(!email) return res.status(400).json({message: "Email is Required!"});
+    if(!password) return res.status(400).json({message: "Password is Required!"});
+    if(password && password.length < 6) return res.status(400).json({message: "Password must have 6 character!"});
+
+    const newUser = new User({
+        name, email, password
+    });
+
+    await newUser.save();
+
+    res.status(200).json({message: "User created!", newUser});
 });
 
-app.get("/users", (req, res) => {
-    const data = [
-        {
-        id: "01",
-        name: "Shahin",
-        location: "Faridpur"
-    },
-    {
-        id: "02",
-        name: "Shahin 2",
-        location: "Faridpur"
-    }
-    ]
-
-    res.status(200).json(data)
+app.listen(PORT, ()=>{
+    console.log(`Server running at http://localhost:${PORT}`);
 });
-
-app.post("/users", (req, res)=> {
-    const {name, location} = req.body;
-    if(!name) return res.status(400).json({message: "Name is required!"});
-    if(!location) return res.status(400).json({message: "Location is required!"});
-
-    const data = {
-        name, location
-    }
-    res.status(201).json({message: "Your account is created!", data})
-});
-
-
-app.listen(8080, ()=>{
-    console.log("App running at port http://localhost:8080")
-})
